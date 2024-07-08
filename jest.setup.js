c5a5743771c3163c8cf27af8715da3bdb46330f0
rlmajor <robert.major@gmail.com>
2024-07-08 16:33:22 -0400

    kkk

--- jest.setup.js
+++ jest.setup.js
@@ -1,28 +1,31 @@
-Object.defineProperty(global.navigator, 'clipboard', {
-    value: {
-      writeText: jest.fn(),
-    },
-    writable: true,
-  });
-
-  const mockAddListener = jest.fn((callback) => callback);
-  global.browser.contextMenus.onClicked.addListener = mockAddListener;
-  global.browser.commands.onCommand.addListener = mockAddListener;
-  
+// First, define the global.browser object with all necessary mocks
 global.browser = {
-    contextMenus: {
-      create: jest.fn(),
-      onClicked: {
-        addListener: jest.fn(),
-      },
+  contextMenus: {
+    create: jest.fn(),
+    onClicked: {
+      addListener: jest.fn(),
     },
-    commands: {
-      onCommand: {
-        addListener: jest.fn(),
-      },
+  },
+  commands: {
+    onCommand: {
+      addListener: jest.fn(),
     },
-    tabs: {
-      query: jest.fn(),
-    },
-    // Add other browser API mocks as needed 
-  };
+  },
+  tabs: {
+    query: jest.fn(),
+  },
+  // Add other browser API mocks as needed
+};
+
+// Then, you can manipulate or use the mocks as needed
+const mockAddListener = jest.fn((callback) => callback);
+global.browser.contextMenus.onClicked.addListener = mockAddListener;
+global.browser.commands.onCommand.addListener = mockAddListener;
+
+// Mocking navigator.clipboard for the entire test suite
+Object.defineProperty(global.navigator, 'clipboard', {
+  value: {
+    writeText: jest.fn(),
+  },
+  writable: true,
+});
