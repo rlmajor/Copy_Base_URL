7899e07da121b57f8a68f7a2bf50542df255f1cc
rlmajor <robert.major@gmail.com>
2024-07-09 20:38:47 -0400

    More testing

--- tests/selenium/selenium_test.js
+++ tests/selenium/selenium_test.js
@@ -6,14 +6,18 @@ if (process.env.FIREFOX_PATH) {
   options.setBinary(process.env.FIREFOX_PATH);
 }
 
+// Explicitly setting the path to Geckodriver
 let serviceBuilder = new firefox.ServiceBuilder('/usr/local/bin/geckodriver');
 
 console.log('Firefox Path:', process.env.FIREFOX_PATH);
-// Corrected line: Directly log the path to geckodriver
 console.log('Geckodriver Path:', '/usr/local/bin/geckodriver');
 
 (async function example() {
-  let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
+  let driver = await new Builder()
+	.forBrowser('firefox')
+	.setFirefoxOptions(options)
+	.setFirefoxService(serviceBuilder) // Ensure the serviceBuilder is correctly utilized
+	.build();
   try {
 	// Your test code here
   } finally {
