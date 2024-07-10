00ae1fb91fc2cac7f91851bc541780f95ffef8fc
rlmajor <robert.major@gmail.com>
2024-07-09 20:23:34 -0400

    updated geckodriver path

--- tests/selenium/selenium_test.js
+++ tests/selenium/selenium_test.js
@@ -6,6 +6,8 @@ if (process.env.FIREFOX_PATH) {
   options.setBinary(process.env.FIREFOX_PATH);
 }
 
+let serviceBuilder = new firefox.ServiceBuilder('/usr/local/bin/geckodriver');
+
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
   try {
