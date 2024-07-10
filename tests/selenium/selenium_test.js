27a43bfa90fae15064567f019be01e91baccf77e
rlmajor <robert.major@gmail.com>
2024-07-09 20:31:00 -0400

    Debug

--- tests/selenium/selenium_test.js
+++ tests/selenium/selenium_test.js
@@ -1,12 +1,16 @@
 const { Builder } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 
+console.log('Firefox Path:', process.env.FIREFOX_PATH);
+console.log('Geckodriver Path:', serviceBuilder.getExecutablePath());
+
 let options = new firefox.Options();
 if (process.env.FIREFOX_PATH) {
   options.setBinary(process.env.FIREFOX_PATH);
 }
 
 let serviceBuilder = new firefox.ServiceBuilder('/usr/local/bin/geckodriver');
+.setStdio('inherit'); // This will print the geckodriver logs to the console.
 
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
