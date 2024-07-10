6d7c757ace0cf5ef6c623641d877744cc06e81fb
rlmajor <robert.major@gmail.com>
2024-07-09 22:05:18 -0400

    trigger

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // Use 'production' for production builds
-  entry: './background.js', // Entry point of your application
+  entry: './background.js', 
   output: {
     filename: 'bundle.js', // Output bundle file name
     path: path.resolve(__dirname, 'dist'), // Output directory
