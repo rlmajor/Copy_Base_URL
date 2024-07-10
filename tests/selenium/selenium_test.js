2d8ae8b5ccf9ec6a4e944b05821fab37e955b2ba
rlmajor <robert.major@gmail.com>
2024-07-09 20:34:25 -0400

    Moving logging after init

--- tests/selenium/selenium_test.js
+++ tests/selenium/selenium_test.js
@@ -1,9 +1,6 @@
 const { Builder } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 
-console.log('Firefox Path:', process.env.FIREFOX_PATH);
-console.log('Geckodriver Path:', serviceBuilder.getExecutablePath());
-
 let options = new firefox.Options();
 if (process.env.FIREFOX_PATH) {
   options.setBinary(process.env.FIREFOX_PATH);
@@ -11,6 +8,9 @@ if (process.env.FIREFOX_PATH) {
 
 let serviceBuilder = new firefox.ServiceBuilder('/usr/local/bin/geckodriver');
 setStdio('inherit'); // This will print the geckodriver logs to the console.
+
+console.log('Firefox Path:', process.env.FIREFOX_PATH);
+console.log('Geckodriver Path:', serviceBuilder.getExecutablePath());
 
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
