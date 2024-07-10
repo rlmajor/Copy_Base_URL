d97932bf768ac5f071a6c8f064c7b85d1c99686b
rlmajor <robert.major@gmail.com>
2024-07-09 20:36:45 -0400

    cp correction

--- tests/selenium/selenium_test.js
+++ tests/selenium/selenium_test.js
@@ -9,13 +9,14 @@ if (process.env.FIREFOX_PATH) {
 let serviceBuilder = new firefox.ServiceBuilder('/usr/local/bin/geckodriver');
 
 console.log('Firefox Path:', process.env.FIREFOX_PATH);
-console.log('Geckodriver Path:', serviceBuilder.getExecutablePath());
+// Corrected line: Directly log the path to geckodriver
+console.log('Geckodriver Path:', '/usr/local/bin/geckodriver');
 
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
   try {
-    // Your test code here
+	// Your test code here
   } finally {
-    await driver.quit();
+	await driver.quit();
   }
 })();
