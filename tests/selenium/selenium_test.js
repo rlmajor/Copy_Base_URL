80da8fddcd6e079cc3aaf89c273d85b142f17bc0
rlmajor <robert.major@gmail.com>
2024-07-09 22:43:25 -0400

    more tweaks

--- tests/selenium/selenium_test.js
+++ tests/selenium/selenium_test.js
@@ -10,11 +10,17 @@ if (process.env.FIREFOX_PATH) {
 options.addArguments("--headless");
 options.addArguments('disable-gpu');
 
+// Load the .xpi extension if the XPI_PATH environment variable is set
+if (process.env.XPI_PATH) {
+  options.addExtensions(process.env.XPI_PATH);
+}
+
 let serviceBuilder = new firefox.ServiceBuilder('/usr/local/bin/geckodriver')
   .setStdio('inherit'); // This will print the Geckodriver logs to the console.
 
 console.log('Firefox Path:', process.env.FIREFOX_PATH);
 console.log('Geckodriver Path:', '/usr/local/bin/geckodriver');
+console.log('XPI Path:', process.env.XPI_PATH); // Log the path to the .xpi file for debugging
 
 (async function example() {
   let driver = await new Builder()
