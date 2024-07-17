c2d670ae914b224f0d7b15dc4a5cbac1fe8b13b0
Robert Major <robert.major@gmail.com>
2024-07-16 21:24:48 -0400

    Updates

--- background.js
+++ background.js
@@ -55,7 +55,7 @@ function createContextMenu() {
 }
 
 // Correctly export the functions
-export default { getBaseURL, copyBaseURL, createContextMenu };
+// export default { getBaseURL, copyBaseURL, createContextMenu };
 
 browser.runtime.onInstalled.addListener(function(details){
   if(details.reason === "install"){
